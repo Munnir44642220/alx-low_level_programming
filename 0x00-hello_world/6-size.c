d73a59fa532005b8a7dfccab59f731e72b566812
munnir44642220 <munnirsurajomuazu@gmail.com>
2023-02-16 15:26:08 -0800

    good

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Comment
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of char: 1 byte(s)\n", sizeof(char));
+	printf("Size of an int: 4 byte(s)\n", sizeof(char));
+	printf("Size of a long int: 4 byte(s)\n", sizeof(char));
+	printf("Size of long long int: 8 byte(s)\n", sizeof(char));
+	printf("Size of a float: 4 byte(s)\n", sizeof(char));
+	return (0);
+}
